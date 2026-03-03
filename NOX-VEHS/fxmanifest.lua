ad19669c40370f50393767c114bd7ae5792aed1a
noxrpgg <noxroleplaygg@gmail.com>
2026-03-03 12:58:32 -0300

    carros pt2

---
+++ NOX-VEHS/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'on'
+files {
+
+	"data/**/**.meta",
+	"stream/*",
+	"stream/**/*",
+	"stream/**/**/*"
+
+}
+
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
